fba828be2abf0282dc76f865dcc144fc0043d529
Derrickkip <derrick.korir@gmail.com>
2020-09-04 19:56:54 +0300

    Add upload section

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+// import ImageUploader from './ImageUploader';
 import './App.css';
+import image from './images/image.svg';
 
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p>The current time is {currentTime}.</p>
-      </header>
+    <div className="uploader">
+      <div class="upload_section_main">
+          <div class="upload_section_minor">
+            <p class="main_text">Upload your image</p> 
+            <p class="helper_text">File should be Jpeg, Png...</p>
+            <div class="upload_section">
+              <img class="upload_svg" src={image}/>
+              <p class="drag_message">Drag & Drop your image here</p>
+            </div>
+            <p class="or">or</p>
+            <button class="button">Choose a file</button>
+          </div>
+      </div>
     </div>
   )
 }
